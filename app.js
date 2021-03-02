1f887ad0ab32794041eaf28ab30da27a0a7edf0c
John McLear <john@mclear.co.uk>
2021-03-02 19:47:12 +0000

    better error handling

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const backends = [{
 
 const proxies = {};
 
+// Create the backends.
 for (const backendId of Object.keys(backends)) {
   /* eslint-disable-next-line new-cap */
   proxies[backendId] = new httpProxy.createProxyServer({
@@ -22,42 +23,26 @@ for (const backendId of Object.keys(backends)) {
   });
 }
 
+// Create the routes for web traffic to those backends.
 const proxyServer = http.createServer((req, res) => {
   const searchParams = new URLSearchParams(req.url);
   const padId = searchParams.get('/socket.io/?padId');
   if (padId === 'test1') {
-    proxies[0].web(req, res);
+    proxies[0].web(req, res, (e) => {
+      console.error(e);
+    });
   }
   if (padId === 'test2') {
-    proxies[1].web(req, res);
+    proxies[1].web(req, res, (e) => {
+      console.error(e);
+    });
   }
 });
-
 proxyServer.on('error', (e) => {
   console.log('proxyserver error', e);
 });
-/*
-proxyOne.on('error', (e, req, res) => {
-  console.log('proxyOne error', e);
-  res.writeHead(500, {
-    'Content-Type': 'text/plain',
-  });
-  res.end('Something went wrong. And we are reporting a custom error message.');
-});
 
-proxyTwo.on('error', (e, req, res) => {
-  console.log('proxyTwo error', e);
-  res.writeHead(500, {
-    'Content-Type': 'text/plain',
-  });
-  res.end('Something went wrong. And we are reporting a custom error message.');
-});
-*/
-
-//
-// Listen to the `upgrade` event and proxy the
-// WebSocket requests as well.
-//
+// Create a route for upgrade / websockets
 proxyServer.on('upgrade', (req, socket, head) => {
   const searchParams = new URLSearchParams(req.url);
   const padId = searchParams.get('/socket.io/?padId');
@@ -69,4 +54,5 @@ proxyServer.on('upgrade', (req, socket, head) => {
   }
 });
 
+// Finally listen on port 9000 :)
 proxyServer.listen(9000);
