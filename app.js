70f0fc25f1573e6f98a4170fd011db819adb5ad1
John McLear <john@mclear.co.uk>
2021-03-04 16:35:33 +0000

    support dynamic backends

--- app.js
+++ app.js
@@ -16,16 +16,56 @@ const backends = {
     host: 'localhost',
     port: 9002,
   },
+  backend3: {
+    host: 'localhost',
+    port: 9003,
+  },
 };
 
 const proxies = {};
 let availableBackend = null;
 setInterval(async () => {
   availableBackend = await checkAvailability(backends, checkInterval, maxPadsPerInstance);
-  console.log(availableBackend)
 }, checkInterval);
 
 const db = new ueberdb.Database('dirty', {filename: './dirty.db'});
+
+const initiateRoute = (backend, req, res, socket, head) => {
+  if (res) {
+    proxies[backend].web(req, res, (e) => {
+      console.error(e);
+    });
+  }
+  if (socket && head) {
+    proxies[backend].ws(req, socket, head, (e) => {
+      console.error(e);
+    });
+  }
+};
+
+const createRoute = (padId, req, res, socket, head) => {
+  // If the route isn't for a specific padID IE it's for a static file
+  // we can use any of the backends but now let's use the first :)
+  if (!padId) {
+    return initiateRoute('backend1', req, res, socket, head);
+  }
+
+  // pad specific backend required, do we have a backend already?
+  db.get(`padId:${padId}`, (e, r) => {
+    if (r && r.backend) {
+      console.log(`database hit: ${padId} <> ${r.backend}`);
+      initiateRoute(r.backend, req, res, socket, head);
+    } else {
+      // if no backend is stored for this pad, create a new connection
+      db.set(`padId:${padId}`, {
+        backend: availableBackend,
+      });
+      console.log(`database miss: ${padId} <> ${availableBackend}`);
+      initiateRoute(availableBackend, req, res, socket, head);
+    }
+  });
+};
+
 db.init(() => {
   // Create the backends.
   for (const backendId of Object.keys(backends)) {
@@ -39,21 +79,16 @@ db.init(() => {
   }
   // Create the routes for web traffic to those backends.
   const proxyServer = http.createServer((req, res) => {
+    let padId;
+    if (req.url.indexOf('/p/') !== -1) {
+      padId = req.url.split('/p/')[1].split('?')[0];
+      console.log(`initial request to /p/${padId}`);
+    }
     const searchParams = new URLSearchParams(req.url);
-    const padId = searchParams.get('/socket.io/?padId');
-    db.get(`padId:${padId}`, (e, r) => {
-      if (r) {
-        proxies[r.backend].web(req, res, (e) => {
-          console.error(e);
-        });
-      } else {
-        // if no backend is stored for this pad, create a new connection
-        db.set(`padId:${padId}`, {
-          backend: availableBackend,
-        });
-      }
-    });
+    padId = searchParams.get('/socket.io/?padId');
+    createRoute(padId, req, res, null, null);
   });
+
   proxyServer.on('error', (e) => {
     console.log('proxyserver error', e);
   });
@@ -62,18 +97,7 @@ db.init(() => {
   proxyServer.on('upgrade', (req, socket, head) => {
     const searchParams = new URLSearchParams(req.url);
     const padId = searchParams.get('/socket.io/?padId');
-    db.get(`padId:${padId}`, (e, r) => {
-      if (r) {
-        proxies[r.backend].ws(req, socket, head, (e) => {
-          console.error(e);
-        });
-      } else {
-        // if no backend is stored for this pad, create a new connection
-        db.set(`padId:${padId}`, {
-          backend: availableBackend,
-        });
-      }
-    });
+    createRoute(padId, req, null, socket, head);
   });
 
   // Finally listen on port 9000 :)
--- checkAvailability.js
+++ checkAvailability.js
@@ -7,13 +7,22 @@ exports.checkAvailability = async (backends, interval, maxPadsPerInstance) => {
     // query if it's free
     const stats = await superagent.get(`http://${backend.host}:${backend.port}/stats`);
     const activePads = JSON.parse(stats.text).activePads || 0;
+    console.log(`${backendId}: ${activePads}`);
     if (activePads === 0) {
       // console.log(`Free backend: ${backend} with ${activePads} active pads`);
+      console.log(`free backend: ${backendId}: ${activePads}`);
       return backendId;
     }
-    if (activePads <= maxPadsPerInstance) {
+    if (activePads < maxPadsPerInstance) {
+      console.log(`available backend: ${backendId}: ${activePads}`);
       return backendId;
+    } else {
+      console.log(`delete backend: ${backendId}: ${activePads}`);
+      delete backends.backendId;
     }
-    // TODO, handle if it hasn't had any available instances where to pass to?
   }
+  // TODO handle no backends available gracefully.
+  if (backends.length === 0) throw new Error('Server full');
+  const randomBackend = Math.floor(Math.random() * Object.keys(backends.length));
+  return randomBackend;
 };
