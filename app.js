81b947109e4c28ed24a35db29e9e95a434c8e03b
John McLear <john@mclear.co.uk>
2021-03-04 15:08:41 +0000

    more dynamic routing logic

--- app.js
+++ app.js
@@ -26,10 +26,38 @@ const proxies = {};
 let availableBackend = null;
 setInterval(async () => {
   availableBackend = await checkAvailability(backends, checkInterval, maxPadsPerInstance);
-  console.log(availableBackend);
 }, checkInterval);
 
 const db = new ueberdb.Database('dirty', {filename: './dirty.db'});
+
+const initiateRoute = (backend, req, res, socket, head) => {
+  if (res) {
+    proxies[backend].web(req, res, (e) => {
+      console.error(e);
+    });
+  }
+  if (socket && head) {
+    proxies[backend].ws(req, socket, head, (e) => {
+      console.error(e);
+    });
+  }
+};
+
+const createRoute = (padId, req, res, socket, head) => {
+  db.get(`padId:${padId}`, (e, r) => {
+    if (r && r.backend) {
+      initiateRoute(r.backend, req, res, socket, head);
+    } else {
+      // if no backend is stored for this pad, create a new connection
+      console.log('availableBackend', availableBackend);
+      db.set(`padId:${padId}`, {
+        backend: availableBackend,
+      });
+      initiateRoute(availableBackend, req, res, socket, head);
+    }
+  });
+};
+
 db.init(() => {
   // Create the backends.
   for (const backendId of Object.keys(backends)) {
@@ -45,19 +73,9 @@ db.init(() => {
   const proxyServer = http.createServer((req, res) => {
     const searchParams = new URLSearchParams(req.url);
     const padId = searchParams.get('/socket.io/?padId');
-    db.get(`padId:${padId}`, (e, r) => {
-      if (r && r.backend) {
-        proxies[r.backend].web(req, res, (e) => {
-          console.error(e);
-        });
-      } else {
-        // if no backend is stored for this pad, create a new connection
-        db.set(`padId:${padId}`, {
-          backend: availableBackend,
-        });
-      }
-    });
+    createRoute(padId, req, res, null, null);
   });
+
   proxyServer.on('error', (e) => {
     console.log('proxyserver error', e);
   });
@@ -66,18 +84,7 @@ db.init(() => {
   proxyServer.on('upgrade', (req, socket, head) => {
     const searchParams = new URLSearchParams(req.url);
     const padId = searchParams.get('/socket.io/?padId');
-    db.get(`padId:${padId}`, (e, r) => {
-      if (r) {
-        proxies[r.backend].ws(req, socket, head, (e) => {
-          console.error(e);
-        });
-      } else {
-        // if no backend is stored for this pad, create a new connection
-        db.set(`padId:${padId}`, {
-          backend: availableBackend,
-        });
-      }
-    });
+    createRoute(padId, req, null, socket, head);
   });
 
   // Finally listen on port 9000 :)
