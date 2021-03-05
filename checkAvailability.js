7350aa9d8dd3d821495236b85dc57899b2261048
John McLear <john@mclear.co.uk>
2021-03-05 15:52:01 +0000

    proxy to next available then random on full

--- checkAvailability.js
+++ checkAvailability.js
@@ -2,13 +2,12 @@
 
 exports.checkAvailability = async (backends, interval, maxPadsPerInstance) => {
   const superagent = require('superagent');
-  const backendIds = Object.keys(backend);
   let available = Object.keys(backends);
   for (const backendId of Object.keys(backends)) {
     const backend = backends[backendId];
     // query if it's free
     const stats = await superagent.get(`http://${backend.host}:${backend.port}/stats`);
-    const activePads = JSON.parse(stats.text).activePads;
+    const activePads = JSON.parse(stats.text).activePads || 0;
     console.log(`${backendId}: ${activePads}`);
     if (activePads === 0) {
       // console.log(`Free backend: ${backend} with ${activePads} active pads`);
@@ -19,10 +18,16 @@ exports.checkAvailability = async (backends, interval, maxPadsPerInstance) => {
       console.log(`available backend: ${backendId}: ${activePads}`);
       return backendId;
     } else {
-      available = available.filter((bcakend) => backend !== backendId);
-      // console.log(`delete backend: ${backendId}: ${activePads}`);
+      available = available.filter((backend) => backend !== backendId);
     }
   }
-  if (available.length) return available[Math.floor(Math.random() * available.length)];
-  return backendIds[Math.floor(Math.random() * backendIds.length)];
+  if (available.length) {
+    // there is an available backend so send it to that..
+    // we should of done this above tho..
+    return available[Math.floor(Math.random() * available.length)];
+  } else {
+    // no available backends so send it to a random backend XD
+    // TODO future, support an error message if no backends are available?
+    return Object.keys(backends)[Math.floor(Math.random() * Object.keys(backends).length)];
+  }
 };
