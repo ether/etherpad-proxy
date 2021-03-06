25432df9c0a52b24fc7467c0b315e2c1d2e82720
John McLear <john@mclear.co.uk>
2021-03-06 19:32:38 +0000

    Update checkAvailability.js

--- checkAvailability.js
+++ checkAvailability.js
@@ -6,31 +6,28 @@ exports.checkAvailability = async (backends, interval, maxPadsPerInstance) => {
   for (const backendId of Object.keys(backends)) {
     const backend = backends[backendId];
     // query if it's free
-    const stats = await superagent.get(`http://${backend.host}:${backend.port}/stats`);
-    const activePads = JSON.parse(stats.text).activePads || 0;
-    if (activePads === 0) {
-      // console.log(`free backend: ${backendId}: ${activePads}`);
-      return backendId;
-    }
-    if (activePads < maxPadsPerInstance) {
-      // console.log(`available backend: ${backendId}: ${activePads}`);
-      return backendId;
-    } else {
+    try {
+      const stats = await superagent.get(`http://${backend.host}:${backend.port}/stats`);
+      const activePads = JSON.parse(stats.text).activePads || 0;
+      if (activePads === 0) {
+        // console.log(`free backend: ${backendId}: ${activePads}`);
+        return backendId;
+      }
+      if (activePads < maxPadsPerInstance) {
+        // console.log(`available backend: ${backendId}: ${activePads}`);
+        return backendId;
+      } else {
+        available = available.filter((backend) => backend !== backendId);
+      }
+    } catch (e) {
+      // console.log(`removing backend: ${backendId}`);
       available = available.filter((backend) => backend !== backendId);
     }
   }
   if (available.length) {
     // there is an available backend so send it to that..
-    // we should of done this above tho..
     const randomBackend = available[Math.floor(Math.random() * available.length)];
     // console.log(`random available backend: ${randomBackend}`);
-    return randomBackend;
-  } else {
-    // no available backends so send it to a random backend XD
-    // TODO future, support an error message if no backends are available?
-    const randomBackend =
-        Object.keys(backends)[Math.floor(Math.random() * Object.keys(backends).length)];
-    // console.log(`Full up: random backend: ${randomBackend}`);
     return randomBackend;
   }
 };
